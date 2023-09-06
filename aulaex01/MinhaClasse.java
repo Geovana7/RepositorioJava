6c36e06b760fb7c95e53a0da57c88032725afb87
Geovana7 <141193054+Geovana7@users.noreply.github.com>
2023-09-06 19:18:52 -0300

    Add files via upload

---
+++ aulaex01/MinhaClasse.java
@@ -0,0 +1,17 @@
+
+package minhaclasse;
+
+/**
+ *Exercício 01
+ * Aula:01/08/23
+ * Disciplina:Linguagem de Programação
+ * @author Geovana
+ */
+public class MinhaClasse {
+
+   //Meu primeiro programa Java
+    public static void main(String[] args) {
+      System.out.println("Olá Mundo!");
+    }
+    
+}
---
+++ aulaex02/SegundaClasse.java
@@ -0,0 +1,26 @@
+
+package segundaclasse;
+
+/**
+ *Exercício 02
+ * Aula:01/08/23
+ * Disciplina:Linguagem de Programação
+ * Materia:Manipulação de String
+ * @author 15671393612
+ */
+public class SegundaClasse {
+ public static void main(String[] args){
+  String msg1 = "Olá";
+  String msg2 = "Geovana";
+  
+  String mensagem = msg1 + "," + msg2 + "!";
+  System.out.println(mensagem);
+  
+  String texto = "Hello, Word!";
+  int tamanho = texto.length();
+  System.out.println(tamanho);
+  String txt ="Hello Word";
+  System.out.println(txt.toUpperCase());
+  System.out.println(txt.toLowerCase());
+ }   
+}
